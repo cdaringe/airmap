d7fd3bf437234dd63e829c5136b712f84ef15b5c
c0d01a5 <christopher.dieringer@walmartlabs.com>
2021-10-31 19:04:07 -0700

    chore: disable csv & drive

--- src/components/data-source/DataSourceSelector.tsx
+++ src/components/data-source/DataSourceSelector.tsx
@@ -9,8 +9,8 @@ export enum DataSourceNames {
 export const DataSourceSelector: React.FC<React.HTMLProps<HTMLSelectElement>> =
   (props) => (
     <Select {...props}>
-      <option value={DataSourceNames.googleDrive}>Google Drive</option>
+      {/* <option value={DataSourceNames.googleDrive}>Google Drive</option> */}
       <option value={DataSourceNames.googlesheetsurl}>Google Sheets URL</option>
-      <option value={DataSourceNames.csvurl}>CSV URL</option>
+      {/* <option value={DataSourceNames.csvurl}>CSV URL</option> */}
     </Select>
   );
