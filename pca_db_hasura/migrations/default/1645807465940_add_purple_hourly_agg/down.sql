7ba6f6a37bb452e766b22f0ea745070fcb6bd771
cdaringe <cdaringe@gmail.com>
2022-02-25 08:47:42 -0800

    chore: add high perf agg

---
+++ pca_db_hasura/migrations/default/1645807465940_add_purple_hourly_agg/down.sql
@@ -0,0 +1,15 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE VIEW observation_purpleair_hourly_agg as (
+--     SELECT
+--       time_bucket('60 minutes', timestamp) AS bucket,
+--       sensor_id,
+--       min(timestamp) as timestamp,
+--       avg(pm_2_5_atm) as avg_pm_2_5_atm,
+--       avg(humidity) as avg_humidity,
+--       avg(temperature_f) as avg_temperature_f,
+--       avg(pm_2_5_cf) as avg_pm_2_5_cf,
+--       avg(pm_1_atm) as avg_pm_1_atm
+--     FROM observation_purpleair
+--     GROUP BY bucket, sensor_id
+-- );
