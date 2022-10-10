2d4aaaab5a5ae67e100ba6de472280983b50616f
cdaringe <cdaringe@gmail.com>
2022-10-09 23:39:28 -0700

    docs: doc block

--- apps/airmap/deno-compat-loader.js
+++ apps/airmap/deno-compat-loader.js
@@ -1,8 +1,9 @@
 module.exports = function (source) {
   const callback = this.async();
+  // dude, really? parse the AST
   const nextSrc = source.replaceAll(/\.ts"/gm, '"');
   if (nextSrc !== source) {
-    console.warn(`.ts write ${this.resourcePath}`);
+    console.warn(`[cdaringe-deno-compat-lame] .ts write ${this.resourcePath}`);
   }
   callback(null, nextSrc);
 };
